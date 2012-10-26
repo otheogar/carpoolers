df94d5efb2ff91e090650e7984c06aeb0063e2ef
Oana Theogarajan <oana.catu@gmail.com>
2012-10-25 21:51:14 -0700

    oana - new branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carpoolers::Application.config.secret_token = '9ec222617e7e744b720eef2e9e67071b4702566384155da61a7aab943866a2e21285dfa07b6ef985fa515a0e3f20cafbc9df332524e795e19c52ff3eb6311da4'
