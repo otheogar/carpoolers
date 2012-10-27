82cb0b59fca0d0eca708099fa7f3e7fbfe234dc4
oana <oana@oanaUbuntu.(none)>
2012-10-27 14:50:07 -0700

    oana - facebook login done - removed comments and console logs

--- config/routes.rb
+++ config/routes.rb
@@ -3,26 +3,6 @@ Carpoolers::Application.routes.draw do
 
   get "welcome/index"
 
-  #
-  #controller :sessions do
-  #  get  'login' => :new
-  #  post 'login' => :create
-  #  delete 'logout' => :destroy
-  #end
-
-  #
-  #get '/channel.html' => proc {
-  #  [
-  #      200,
-  #      {
-  #          'Pragma'        => 'public',
-  #          'Cache-Control' => "max-age=#{1.year.to_i}",
-  #          'Expires'       => 1.year.from_now.to_s(:rfc822),
-  #          'Content-Type'  => 'text/html'
-  #      },
-  #      ['<script type="text/javascript" src="//connect.facebook.net/en_US/all.js"></script>']
-  #  ]
-  #}
 
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
