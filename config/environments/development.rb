6c66a6abc37d0906f9cb60bf1381667c4d489606
otheogar <oana.catu@gmail.com>
2012-11-12 16:02:19 -0800

    oana-fixed map

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,12 +29,19 @@ Carpool::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+
+  ####Emulate production
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Expands the lines which load the assets
-  config.assets.debug = true
-  
+  config.assets.debug =  false
+
+  config.serve_static_assets = false
+
+  config.assets.compile = true
+
+
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
   
   
